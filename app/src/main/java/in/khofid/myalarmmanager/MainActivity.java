86c4ee067a1a6a6e90809c94d8a50a9804da423f
Khofidin <offiedz@gmail.com>
2019-07-06 21:48:39 +0700

    Add Alarm Once

--- app/src/main/java/in/khofid/myalarmmanager/MainActivity.java
+++ app/src/main/java/in/khofid/myalarmmanager/MainActivity.java
@@ -2,12 +2,94 @@ package in.khofid.myalarmmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        DatePickerFragment.DialogDateListener, TimePickerFragment.DialogTimeListener {
+
+    TextView tvOnceDate, tvOnceTime;
+    EditText edtOnceMessage;
+    ImageButton btnOnceDate, btnOnceTime;
+    Button btnSetOnce;
+
+    private AlarmReceiver alarmReceiver;
+
+    final String DATE_PICKER_TAG = "DatePicker";
+    final String TIME_PICKER_ONCE_TAG = "TimePickerOne";
+    final String TIME_PICKER_REPEAT_TAG = "TimePickerRepeat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvOnceDate = findViewById(R.id.tv_once_date);
+        tvOnceTime = findViewById(R.id.tv_once_time);
+        btnOnceDate = findViewById(R.id.btn_once_date);
+        btnOnceTime = findViewById(R.id.btn_once_time);
+        edtOnceMessage = findViewById(R.id.edt_once_message);
+        btnSetOnce = findViewById(R.id.btn_set_once_alarm);
+
+        btnOnceDate.setOnClickListener(this);
+        btnOnceTime.setOnClickListener(this);
+        btnSetOnce.setOnClickListener(this);
+
+        alarmReceiver = new AlarmReceiver();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_once_date:
+                DatePickerFragment datePickerFragment = new DatePickerFragment();
+                datePickerFragment.show(getSupportFragmentManager(), DATE_PICKER_TAG);
+                break;
+            case R.id.btn_once_time:
+                TimePickerFragment timePickerFragment = new TimePickerFragment();
+                timePickerFragment.show(getSupportFragmentManager(), TIME_PICKER_ONCE_TAG);
+                break;
+            case R.id.btn_set_once_alarm:
+                String onceDate = tvOnceDate.getText().toString();
+                String onceTime = tvOnceTime.getText().toString();
+                String onceMessage = edtOnceMessage.getText().toString();
+
+                alarmReceiver.setOneTimeAlarm(this, AlarmReceiver.TYPE_ONE_TIME, onceDate, onceTime, onceMessage);
+                break;
+        }
+    }
+
+
+    @Override
+    public void onDialogDataSet(String tag, int year, int month, int dayOfMoth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, dayOfMoth);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+
+        tvOnceDate.setText(dateFormat.format(calendar.getTime()));
+    }
+
+    @Override
+    public void onDialogTimeSet(String tag, int hourOfDay, int minute) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, minute);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+
+        switch (tag) {
+            case TIME_PICKER_ONCE_TAG:
+                tvOnceTime.setText(dateFormat.format(calendar.getTime()));
+                break;
+            default:
+                break;
+        }
     }
 }
